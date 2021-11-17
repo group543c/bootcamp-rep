a1b505dc35a17f57485bad380a516dc5f7a48dde
group543 <group543c@yandex.ru>
2021-11-17 13:40:30 +0900

    Задание 3.
    Сценарий логина

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,6 +1,7 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,7 +19,10 @@ public class MyFirstTest {
 
     @Test
     public void myFirstTest() {
-        driver.navigate().to("https://software-testing.ru/");
+        driver.navigate().to("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
