8e1769dd2697b2998518415c4e204cb1d082c87a
group543 <group543c@yandex.ru>
2021-11-17 14:48:18 +0900

    Запуск в разных браузерах

--- java2/src/test/java/MyFirstTest.java
+++ java2/src/test/java/MyFirstTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MyFirstTest {
 
@@ -13,12 +15,17 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest() {
+        driver.navigate().to("http://google.com");
+
+    }
+    @Test
+    public void mySecondTest() {
         driver.navigate().to("http://localhost/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
