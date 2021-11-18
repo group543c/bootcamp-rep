e848b483bdfe6b5c463d3172013ac1c50e604ebb
group543 <group543c@yandex.ru>
2021-11-18 13:57:16 +0900

    del build

--- java2/src/test/java/MyFirstTest.java
+++ java2/src/test/java/MyFirstTest.java
@@ -2,9 +2,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -15,17 +19,17 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void myFirstTest() {
+    public void myFirstTest() { //Задание 1
         driver.navigate().to("http://google.com");
 
     }
     @Test
-    public void mySecondTest() {
+    public void mySecondTest() { //Задание 3 логин
         driver.navigate().to("http://localhost/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
